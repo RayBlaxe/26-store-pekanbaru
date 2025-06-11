6401fc1a5575ba6e64f845cdaf27492a52499153
RayBlaxe <raihan21ti@mahasiswa.pcr.ac.id>
2025-06-11 23:58:35 +0700

    Add a Logo tab header

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: '26 Store Pekanbaru - E-Commerce Olahraga',
+  description: 'Ingat Olahraga Ingat 26 Store Pekanbaru',
+  generator: 'aza-dev',
 }
 
 export default function RootLayout({
